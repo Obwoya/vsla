e04e4015b03ab74a36e63b2e0e66869a86531e24
shyakadev <shyakadev@gmail.com>
2018-02-21 09:19:17 +0200

    moved the signup form

--- views/header.php
+++ views/header.php
@@ -23,7 +23,7 @@
               <a class="col-md-6" href="?controller=account&action=index"><h1 style="color: white; font-size: 16px">VILLAGE SAVINGS AND LOANS ASSOCIATION (VSLA)</h1></a>
               <?php if(!isset($_SESSION['type'])){ ?>
 <div class="text-right" style="font-size: 20px; margin-top: 10px;">
-<a style="color: white; padding-right: 20px" href="?page=signup">Signup</a>
+<a style="color: white; padding-right: 20px" href="?page=signup">Sign-Up</a>
 <a style="color: white;" href="?controller=account&action=index">Login</a>
 </div>
 <?php } else {} ?>
